4e6d509705925e1ea4a13e627c18e78bdab7a84f
marcvan12 <marc@realmotor.jp>
2025-07-07 19:42:13 +0900

    Change Database +4

--- src/lib/mongodbClient.js
+++ src/lib/mongodbClient.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.NEXT_PUBLIC_MONGODB_URI ? process.env.NEXT_PUBLIC_MONGODB_URI : process.env.API_KEY
+const uri = process.env.NEXT_PUBLIC_MONGODB_URI ? process.env.NEXT_PUBLIC_MONGODB_URI : "mongodb://realmototorjapan-read:c8dvZIF2H8fvzVSSCkrNeSH692cFymAHgYFAKvGGv8fJOZXI@758f241f-9cca-4dbd-a976-e89a49a78ce3.asia-northeast2.firestore.goog:443/mongodb-prod?loadBalanced=true&tls=true&authMechanism=SCRAM-SHA-256&retryWrites=false"
 const dbName = "mongodb-prod";
 
 let cachedClient = global._mongoClient;
