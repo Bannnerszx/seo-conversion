374aead5862bee7dc622ac9410dd0f99ec00f180
marcvan12 <marc@realmotor.jp>
2025-12-05 15:10:04 +0900

    SEO and speed update +9.2.6

--- src/app/homeComponents/HeroBanner.js
+++ src/app/homeComponents/HeroBanner.js
@@ -31,7 +31,6 @@ export default function HeroBanner({ unsoldVehicleCount }) {
           sizes="100vw"
           className="h-full w-full object-cover object-center"
           priority={true} // 'priority' already includes fetchPriority="high"
-          placeholder="blur"
           quality={40} 
         />
         </div>
